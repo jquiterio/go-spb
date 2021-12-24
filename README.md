1508a9b5bb66a09987ebc7d8c73e63dc41d077c5
Jorge Quiterio <jquiterio00@gmail.com>
2021-12-24 17:09:16 +0100

    gencert created

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Simple PubSub for Go
+
+---
--- gencert.sh
+++ gencert.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 openssl genrsa -out ca.key 2048
-openssl req -new -x509 -key ca.key -out ca.pem -days 3650 -subj '/CN=ca'
+openssl req -new -x509 -key ca.key -out ca.pem -days 3650 -subj '/CN=root ca'
 openssl genrsa -out server.key 2048
-openssl req -new -key server.key -out server.csr -subj '/CN=server'
-openssl x509 -req -in server.csr -CA ca.pem -CAkey ca.key -CAcreateserial -out server.pem -days 3650
+openssl req -newkey rsa:2048 -nodes -keyout server.key -subj "/CN=localhost" -out server.csr
+openssl x509 -req -extfile <(printf "subjectAltName=DNS:localhost") -days 365 -in server.csr -CA ca.pem -CAkey ca.key -CAcreateserial -out server.pem
 openssl genrsa -out client.key 2048
-openssl req -new -key client.key -out client.csr -subj '/CN=client'
+openssl req -new -key client.key -out client.csr -subj '/CN=localhost' 
 openssl x509 -req -in client.csr -CA ca.pem -CAkey ca.key -CAcreateserial -out client.pem -days 3650
