1e156902897ebdb38b8ab75826d94036e40fddf6
Jorge Quiterio <jquiterio00@gmail.com>
2022-01-25 13:14:13 +0100

    ...

--- config/config.go
+++ config/config.go
@@ -25,56 +25,6 @@ type Config struct {
 	}
 }
 
-// var Config = struct {
-// 	Hub struct {
-// 		Addr   string
-// 		Port   string
-// 		Secure bool
-// 	}
-// 	Redis struct {
-// 		Addr   string
-// 		Passwd string
-// 		DB     int
-// 	}
-// }{}
-
-// func init() {
-// 	hub_addr := os.Getenv("HUB_ADDR")
-// 	if hub_addr == "" {
-// 		Config.Hub.Addr = "localhost"
-// 	} else {
-// 		Config.Hub.Addr = hub_addr
-// 	}
-// 	hub_port := os.Getenv("HUB_PORT")
-// 	if hub_port == "" {
-// 		Config.Hub.Port = "8083"
-// 	} else {
-// 		Config.Hub.Port = hub_port
-// 	}
-// 	if hub_secure, err := strconv.ParseBool(os.Getenv("HUB_SECURE")); err == nil {
-// 		Config.Hub.Secure = hub_secure
-// 	} else {
-// 		Config.Hub.Secure = false
-// 	}
-// 	redis_addr := os.Getenv("REDIS_ADDR")
-// 	if redis_addr == "" {
-// 		Config.Redis.Addr = "localhost:6379"
-// 	} else {
-// 		Config.Redis.Addr = redis_addr
-// 	}
-// 	redis_db := os.Getenv("REDIS_DB")
-// 	if redis_db == "" {
-// 		Config.Redis.DB = 0
-// 	} else {
-// 		db, err := strconv.Atoi(redis_db)
-// 		if err != nil {
-// 			Config.Redis.DB = 0
-// 		} else {
-// 			Config.Redis.DB = db
-// 		}
-// 	}
-// }
-
 func GetFromEnvOrDefault() *Config {
 
 	config := Config{}
